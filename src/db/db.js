ce93773f4c1b31d90ed79f47dd6f6e7f2aa94402
satwika <chitrasatwika@gmail.com>
2026-03-12 15:57:06 +0530

    deleted entire project because of an error

--- src/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectDB() {
-    await mongoose.connect("mongodb+srv://satwika:Benstokes%4055@cluster0.9vbvg1d.mongodb.net/?appName=Cluster0")
-    console.log("Connected to MongoDB");
-}
-
-module.exports = connectDB;
